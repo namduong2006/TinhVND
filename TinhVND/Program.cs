000394d3805c86e06cce0eaf047007d98ac7517c
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-02 16:10:01 +0700

    Add project files.

---
+++ TinhVND/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhVND
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;           
+            float uSD,vND;
+            Console.WriteLine("Chuyển đổi USD sang VND");
+            Console.Write("Mời nhập số tiền cần đổi:");
+            uSD = float.Parse(Console.ReadLine());
+            vND = uSD * 23000;
+            Console.WriteLine("Số tiền nhận được:" +vND+" VND");
+            Console.ReadLine();
+
+
+        }
+    }
+}
